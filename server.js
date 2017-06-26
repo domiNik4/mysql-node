a23a5574c6c4588929952a27d4362abee875220d
domiNik4 <dominika.fric@akademiasovy.sk>
2017-06-26 11:12:09 +0200

    backend done

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+const mysql=require('mysql');
+const connectionManager=require('mysql-connection-manager');
+var app =express();
+app.use(bodyParser.json());
+
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:"school"
+});
+
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log("Successfully connected to MYSQL database!");
+});
+
+app.get('/',(req,res)=>{
+    res.send('hello');
+});
+
+app.get('/children',(req,res)=>{
+	var query = 'select * from children';
+	conn.query(query, function (err, result) {
+		conn.end();
+    	if (err){
+    		res.status(400).send(err);
+    	}
+    	var children=JSON.stringify(result);
+    	res.status(200).header('content-type','application/json').send(children);
+  	});
+});
+
+app.post('/children',(req,res)=>{
+	var child={name:req.body.name,idp:req.body.idp,status:null}
+	var query = 'insert into children(name, idp) values("'+child.name+'", '+child.idp+')';
+	conn.query(query,function (err, result){
+    	if (err)
+    		res.status(400).send(err);
+    	child.status='inserted';
+    	var msg=JSON.stringify(child);
+    	res.status(200).header('content-type','application/json').send(child);
+    });
+});
+
+app.listen(3000,()=>{console.log("Server running on port 3000. Press ctrl+C to exit.")});
